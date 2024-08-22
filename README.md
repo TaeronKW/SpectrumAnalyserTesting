2599c4871225e138a0c9848583c500a373b4bd73
Kaan Williams <kaan.williams@taeron.ca>
2024-08-22 13:25:44 -0700

    Improved documentation
    Updated readme with usage instructions.
    Added lots of comments.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # SpectrumAnalyserTesting
 Python code for testing the RFExplorer IoT module WSUB3G
+
+This code uses the [RFExplorer Python Library.](https://github.com/RFExplorer/RFExplorer-for-Python) Version 1.33.2106.3 contains some errors that are corrected in my fork which is [here.](https://github.com/TaeronKW/RFExplorer-for-Python) If the errors get corrected in the original repo my fork becomes redundant and the original repo can be installed using the pip3 installer rather than the instructions below.
+
+- Unisntall the existing pip3 librarty if necessary
+  - `sudo pip3 uninstall RFExplorer`
+- Clone the forked RF Explorer library into this project folder
+  - `git clone https://github.com/TaeronKW/RFExplorer-for-Python.git`
+- Create a link to the Library
+  - `ln -s RFExplorer-for-Python/RFExplorer/ RFExplorer`
+- when running the python code, you must be super user for the raspberry pi to properly access IO. Use the -E switch to copy in environment variables if you want to use and plotting functions over ssh/X-term.
+  - `sudo -E python3 fullscan.py`
+
--- fullscan.py
+++ fullscan.py
@@ -1,19 +1,18 @@
 import time
+import traceback
 from datetime import datetime
 import RFExplorer
 from RFExplorer import RFE_Common
-from RFExplorer.RFEConfiguration import RFEConfiguration
-from RFExplorer.RFESweepDataCollection import RFESweepDataCollection
-
 
 BAUDRATE = 500000
 
-# Constants for the frequency
+# Constants for the frequency spectrum definition
 POINTS_PER_SCAN = 2000		# points per scan
 FREQ_PER_POINT_MHZ = 0.1	# 100 kHz intervals
-SCAN_SPAN_MHZ = POINTS_PER_SCAN * FREQ_PER_POINT_MHZ	#
+SCAN_SPAN_MHZ = POINTS_PER_SCAN * FREQ_PER_POINT_MHZ
 
 def connectToAnalyser():
+    # Open a connection to the device as per the IoT module examples
     rfe = RFExplorer.RFECommunicator()
     rfe.AutoConfigure = False
     rfe.GetConnectedPorts()
@@ -26,60 +25,76 @@ def connectToAnalyser():
 
 def main():
     try:
-        print("open")
         rfe = connectToAnalyser()
         if (rfe == None):
             return
-
-
+        
+        # Download device details from the IoT module
         rfe.SendCommand_RequestConfigData()
         while rfe.ActiveModel == RFE_Common.eModel.MODEL_NONE:
             bDraw,rString=rfe.ProcessReceivedString(True)
 
-
+        # Set the initial frequency scan parameters
         start_freq_MHz = rfe.MinFreqMHZ
         stop_freq_MHz = start_freq_MHz + SCAN_SPAN_MHZ
         numPoints = POINTS_PER_SCAN
 
+        # Note that due to issues with the way the RFExplorer IoT module handles the 
+        # setting of data points it is not possible to fully generallise the command 
+        # structure for setting this value (SendCommand_SweepDataPointsEx vs 
+        # SendCommand_SweepDataPoints). This has been submitted as an issue to the 
+        # RFExplorer-for-Python repo.
 
+        # The number of points per scan is constant until the final scan which has a different span
+        rfe.SendCommand_SweepDataPoints(numPoints)
 
         while (stop_freq_MHz <= rfe.MaxFreqMHZ):
+            # Remove existing sweep data
             rfe.CleanSweepData()
+
+            # For the final pass expand the span and increase the number of points to maintain a 
+            # constant number of points per MHz
             if ((stop_freq_MHz + SCAN_SPAN_MHZ) > rfe.MaxFreqMHZ):
                 stop_freq_MHz=rfe.MaxFreqMHZ
                 numPoints = int((stop_freq_MHz-start_freq_MHz) / FREQ_PER_POINT_MHZ)
                 rfe.SendCommand_SweepDataPointsEx(numPoints)
-            else:
-                rfe.SendCommand_SweepDataPoints(numPoints)
-            
+
+            # There is a "quirk" in the device where setting the number of data points causes the Start 
+            # and Stop frequencies to be reset. For this reason an UpdateDeviceConfig command MUST follow 
+            # a change in the number of data points.
             rfe.UpdateDeviceConfig(start_freq_MHz, stop_freq_MHz)
             
+            # Wait for the device to confirm it has received the new parameters. No error checking is being 
+            # done here by this code.
             while True:
                 bDraw,rString=rfe.ProcessReceivedString(True)
                 if (start_freq_MHz == rfe.StartFrequencyMHZ):
                     break
             
-            print(f"Start:{rfe.StartFrequencyMHZ:4.1f} MHz, End:{rfe.StopFrequencyMHZ:4.1f} MHZ, Points:{rfe.FreqSpectrumSteps+1}, Size:{rfe.StepFrequencyMHZ} MHz")
+            # Dsiplay the new settings to the screen. The user is informed of the realised setting so they 
+            # can observe errors.
+            print(f"Start:{rfe.StartFrequencyMHZ:6.1f} MHz, End:{rfe.StopFrequencyMHZ:6.1f} MHZ, Points:{rfe.FreqSpectrumSteps+1}, Size:{rfe.StepFrequencyMHZ} MHz")
             
-            filename = f"scan_{rfe.StartFrequencyMHZ:04.0f}MHz-{rfe.StopFrequencyMHZ:04.0f}MHz_{rfe.FreqSpectrumSteps+1:04.0f}pps.csv"
-            filename
-            
+            # Remove existing sweep data and collect scan data
             rfe.CleanSweepData()
             while (rfe.SweepData.Count < 1):
                 bDraw,rString=rfe.ProcessReceivedString(True)
             
-            rfe.SweepData.GetData(0).SaveFileCSV(filename,",",None)
+            # Choose a filename based on the scan parameters and save the data
+            filename = f"scan_{rfe.StartFrequencyMHZ:04.0f}MHz-{rfe.StopFrequencyMHZ:04.0f}MHz_{rfe.FreqSpectrumSteps+1:04.0f}pps.csv"
+            rfe.SweepData.GetData(0).SaveFileCSV(filename, ",", None)
             
+            # Set the frequncy limits for the next loop
             start_freq_MHz = stop_freq_MHz
             stop_freq_MHz = start_freq_MHz + SCAN_SPAN_MHZ
-
+        # This is the end of the loop "while (stop_freq_MHz <= rfe.MaxFreqMHZ)"
 
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"\nAn error occurred: {e}\n")
+        traceback.print_exc()
     finally:
         rfe.Close()
         rfe = None
-
 
 if __name__ == "__main__":
     main()
